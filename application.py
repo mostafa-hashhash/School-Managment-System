bdb2477aa6bdba70d70467421b3c2dfde1d69ebe
Mostafa Hashhash <mostafasaeed483@gmail.com>
2019-09-22 19:04:49 +0200

    .

--- application.py
+++ application.py
@@ -1,54 +1,111 @@
 import os
-from flask import Flask, render_template,url_for,session,request
+from flask import Flask, render_template,url_for,session,request, redirect
 from models import * 
+from flask_session import Session
+from tempfile import mkdtemp
 
 app = Flask(__name__)
+
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
+
 with app.app_context():
 	db.create_all()
 
-app.Debug = True
+
+
+
+
 
 @app.route("/")
 def home():
-	return render_template("home.html")
+	return render_template("login.html")
 	
 @app.route("/finance")
 def finance():
+	"""
+		info about the payment status
+		paid , not-yet
+	"""
 	return render_template("finance.html")
 	
 @app.route("/follow")
 def follow():
-	return render_template("follow.html")
+	"""
+		info about every lecture  
+		will join the tables to get the student status at this session 
+	"""
+	lecture.query.filter_by(user_id = session["uid"]).all
+	user.query.filter_by(id = session["uid"]).first().lecture
+	db.session.query
+	x = lecture.query.all()
+	return render_template("follow.html",lectures=x)
 	
+
+
+
+
 @app.route("/delay")
 def delay():
+	"""
+		Join The session,student Tables to get all the session for that specific user
+		user data is provided in the session 
+	"""
 	return render_template("delay.html")
+
+
+
+
 
 @app.route("/logout")
 def logout():
-	return render_template("logout.html")
+	key_list = list(session.keys())
+	for key in key_list:
+		session.pop(key)
+	return render_template("lgoin.html")
+
+
+
+
 
 
 @app.route("/login",methods=["GET", "POST"])
 def login():
+
 	if request.method == "POST":
-		user_name = request.form['name']
+		user_name = request.form.get('name')
 		password = request.form.get('password') 
-		login = teacher.query.filter_by(name=user_name, password=password).first()
-		if login is not None:
+		loged = teacher.query.filter_by(name=user_name, password=password).first()
+		if loged is not None:
+			session["user_name"] = user_name
+			session["password" ] = password
+			session["uid"] = loged.id
+			return render_template('home.html',student=loged)
+	else:
+		if "user_name" not in session and "password" not in session :
+			return render_template("login.html")
+		else:
+			user_name = session["user_name"]
+			password = session["password"]
+			login = teacher.query.filter_by(name=user_name, password=password).first()
 			return render_template('home.html',student=login)
 
-	return render_template("login.html")
+
+
+
+
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
 	if request.method == "POST":
-		user_name = request.form['name']
+		user_name = request.form.get('name')
 		password = request.form.get('password') 
 		email = request.form.get('email')
 		phone = request.form.get('phone')
--- models.py
+++ models.py
@@ -1,43 +1,56 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
+
+
+
+
+
 class user(db.Model):
 	__tablename__ = "user"
 	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(50))
-	age =db.Column(db.Integer)
-	phone =db.Column(db.Integer)
-	email = db.Column(db.String(50))
-	password = db.Column(db.String(50))
+	name = db.Column(db.String(50),nullable = False)
+	age =db.Column(db.Integer,nullable = False)
+	phone =db.Column(db.Integer,nullable = False)
+	email = db.Column(db.String(50),nullable = False)
+	password = db.Column(db.String(50),nullable = False)
+	status = db.Column(db.String(10),nullable = False)
 
-	def __init__ (self,name,password,email,phone, age):
+
+	def __init__ (self,name,password,email,phone,age,status):
 		user.password =  password
 		user.phone = phone
 		user.age = age
 		user.name = name
 		user.email = email
+		user.status = status
 	
 	def add_user(name,password,email,age,phone):
-		u = user(name=name,age=age,email=email,phone=phone,password=password)
+		u = user(name=name,age=age,email=email,phone=phone,password=password,status=status)
 		db.session.add(u)
 		db.session.commit()
 
-class session(db.Model):
-	__tablename__ = "session"
+
+
+
+class lecture(db.Model):
+	__tablename__ = "lecture"
 	id = db.Column(db.Integer,primary_key=True)
 	title = db.Column(db.String(40), nullable = False)
 	content = db.Column(db.String(100),nullable = False)
 	date = db.Column(db.Time(40),nullable = False)
 
 	def __init__ (self,title,content,date):
-		session.title =  title
-		session.content = content
-		session.date = date
+		lecture.title =  title
+		lecture.content = content
+		lecture.date = date
 	
-	def add_session(title,content,date):
-		u = Session(title=title,content=content,date=date)
-		db.session.add(u)
-		db.session.commit()
+	def add_lecture(title,content,date):
+		u = lecture(title=title,content=content,date=date)
+		db.lecture.add(u)
+		db.lecture.commit()
+
+
 
 
 class teacher(db.Model):
@@ -62,8 +75,21 @@ class teacher(db.Model):
 		db.session.commit()
 """
 
+
+
+
 class admin(db.Model):
 	__tablename__ = "admin"
 	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(50))
-	password = db.Column(db.String(50))
+	name = db.Column(db.String(50),nullable = False)
+	password = db.Column(db.String(50),nullable = False)
+"""
+	def __init__ (self,name,password):
+		teacher.name =  name
+		teacher.password = password
+		
+	def add_admib(name,password):
+		u = admin(name=name,password=password)
+		db.session.add(u)
+		db.session.commit()
+"""
